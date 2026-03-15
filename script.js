711378e88265437b580722bae26001df1716f48c
Dusk <roseduskwolf@gmail.com>
2026-03-15 19:15:56 -0400

    1.0.2
    Changed most if not all branding to be proper

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 (() => {
   // Twitch embed requires parent=yourdomain.
   // We auto-detect the current hostname so it works on:
-  // - mercuryfallen.org
-  // - www.mercuryfallen.org
+  // - ivyrosewolf.org
+  // - www.ivyrosewolf.org
   // - local testing (localhost)
-  const host = window.location.hostname || "mercuryfallen.org";
+  const host = window.location.hostname || "ivyrosewolf.org";
   const parent = encodeURIComponent(host);
 
-  const channel = "mercuryfallen";
+  const channel = "ivy_rose_wolf";
   const src = `https://player.twitch.tv/?channel=${channel}&parent=${parent}&muted=true`;
 
   const iframe = document.getElementById("twitchEmbed");
